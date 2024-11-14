2767cd92e8be39477d83287b4afd538738e15f1f
zongye0615 <zongyexie@gmail.com>
2024-11-14 13:58:19 +0000

    提交訊息

---
+++ 41323151.py
@@ -0,0 +1,12 @@
+import keyword
+
+# 查看所有 Python 關鍵字
+print("Python 關鍵字: ", keyword.kwlist)
+
+# 正確的變數命名
+user_name = "Alice"
+userAge = 25
+_userHeight = 170
+print("Name:", user_name)
+print("Age:", userAge)
+print("Height:", _userHeight)
